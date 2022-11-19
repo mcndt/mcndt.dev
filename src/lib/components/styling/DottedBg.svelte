4bbd7d40ebe2ef1781d4fe43f731d7fcf4188e8d
Maxime Cannoodt <maxime@qargo.io>
2022-11-19 21:10:15 +0100

    update about me

--- src/lib/components/styling/DottedBg.svelte
+++ src/lib/components/styling/DottedBg.svelte
@@ -9,7 +9,7 @@
 <div
 	class="{cls} 
 	layout-container
-	bg-[radial-gradient(circle_at_12px_12px,_#a3a3a3_1px,_white_0)]
+	bg-[radial-gradient(circle_at_12px_12px,_#737373_1px,_white_0)]
 	pb-1 
 	dark:bg-[radial-gradient(circle_at_12px_12px,_#78716c_1px,_#0a0a0a_0)]
 	"
