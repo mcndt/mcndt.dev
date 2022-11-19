e67d66ab9c60e3539bc9228c714664b43cbd819a
Maxime Cannoodt <maxime@qargo.io>
2022-11-19 21:07:18 +0100

    update bg

--- src/lib/components/styling/DottedBg.svelte
+++ src/lib/components/styling/DottedBg.svelte
@@ -7,15 +7,18 @@
 </script>
 
 <div
-	class="{cls} layout-container
-bg-[radial-gradient(circle_at_1px_1px,_black_1px,_white_0)] pb-1
-dark:bg-[radial-gradient(circle_at_1px_1px,_#78716c_1px,_#0a0a0a_0)]"
+	class="{cls} 
+	layout-container
+	bg-[radial-gradient(circle_at_12px_12px,_#a3a3a3_1px,_white_0)]
+	pb-1 
+	dark:bg-[radial-gradient(circle_at_12px_12px,_#78716c_1px,_#0a0a0a_0)]
+	"
 >
 	<slot />
 </div>
 
 <style>
 	.layout-container {
-		background-size: 28px 28px;
+		background-size: 24px 24px;
 	}
 </style>
