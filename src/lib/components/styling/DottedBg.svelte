1b6913d40a4c4700a3dbcc789c2abe372c167dae
Maxime Cannoodt <maxime@qargo.io>
2023-07-08 09:07:02 +0200

    wip

--- src/lib/components/styling/DottedBg.svelte
+++ src/lib/components/styling/DottedBg.svelte
@@ -6,19 +6,27 @@
 	export { cls as class };
 </script>
 
-<div
-	class="{cls} 
-	layout-container
-	bg-[radial-gradient(circle_at_12px_12px,_#737373_1px,_white_0)] 
-	pb-1
-	dark:bg-[radial-gradient(circle_at_12px_12px,_#78716c_1px,_#0a0a0a_0)]
-	"
->
+<div class="{cls} layout-container pb-1">
 	<slot />
 </div>
 
-<style>
+<style lang="scss" global>
+	.dark {
+		.layout-container {
+			--dot-grid-color: #78716c;
+			--dot-grid-bg-color: #0a0a0a;
+		}
+	}
 	.layout-container {
-		background-size: 24px 24px;
+		--dot-grid-color: #a5a3a3;
+		--dot-grid-bg-color: #ffffff;
+		--dot-grid-size: 24px;
+
+		background-size: var(--dot-grid-size) var(--dot-grid-size);
+		background-image: radial-gradient(
+			circle at calc(var(--dot-grid-size) / 2) calc(var(--dot-grid-size) / 2),
+			var(--dot-grid-color) 1px,
+			var(--dot-grid-bg-color) 0
+		);
 	}
 </style>
